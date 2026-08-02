55793e15f2f3808e85bd4ad8f2b1f78b5651a488
Lukas Hahn <lukas1h07@gmail.com>
2026-08-02 15:47:34 -0700

    Redirects for qr

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  async redirects() {
+    return [
+      {
+        source: "/r",
+        destination: "/real-estate",
+        permanent: false,
+      },
+      {
+        source: "/c",
+        destination: "/real-estate",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
